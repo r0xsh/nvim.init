52a1fc40800c98340e3b4eac6396d5b45c859bf9
Antoine Bagnaud <bagnaud.antoine@gmail.com>
2024-12-11 02:12:23 +0100

    add hl ident

---
+++ lua/plugins/hlindent.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'nvimdev/indentmini.nvim',
+    config = function()
+      require('indentmini').setup { only_current = true }
+      -- vim.cmd.highlight('IndentLine guifg=#333333')
+      vim.cmd.highlight('IndentLineCurrent guifg=#333333')
+    end,
+  }
+}
