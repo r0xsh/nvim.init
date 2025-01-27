80b89204112a95456d9f67b0e9c0837f33726113
Antoine Bagnaud <contact@antoinebagnaud.me>
2025-01-27 21:44:42 +0100

    enable back format_on_save

--- init.lua
+++ init.lua
@@ -545,10 +545,10 @@ require('lazy').setup {
     'stevearc/conform.nvim',
     opts = {
       notify_on_error = true,
-      -- format_on_save = {
-      --   timeout_ms = 500,
-      --   lsp_fallback = true,
-      -- },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
