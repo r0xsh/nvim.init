a7b841b143caa817ec1d7dc4e461687cbba67691
Antoine Bagnaud <contact@antoinebagnaud.me>
2025-01-28 16:52:36 +0100

    add hover.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -107,4 +107,16 @@ return {
   },
   -- { 'elkowar/yuck.vim', ft = 'yuck' },
   -- { 'rescript-lang/vim-rescript', ft="rescript" },
+  {
+    'lewis6991/hover.nvim',
+    config = function()
+      require('hover').setup {
+        init = function()
+          require 'hover.providers.lsp'
+          require 'hover.providers.gh'
+        end,
+      }
+      vim.keymap.set('n', 'K', require('hover').hover, { desc = 'hover.nvim' })
+    end,
+  },
 }
