dfa34a760fe5ec17c50d2eff7c8a47636265434d
Antoine Bagnaud <contact@antoinebagnaud.me>
2025-02-04 00:41:05 +0100

    disable lualine on neo-tree buffer

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -84,6 +84,7 @@ return {
             tabline = 1000,
             winbar = 1000,
           },
+          disabled_filetypes = { 'neo-tree' },
         },
         sections = {
           lualine_c = { 'filename', marlin_component },
