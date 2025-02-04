1a4de85d08da7aa02c56c4ec68e565097e031d51
Antoine Bagnaud <contact@antoinebagnaud.me>
2025-02-04 01:18:01 +0100

    hide EOB ~ on neo-tree buffers

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,17 +1,28 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
+local function hide_eob(args)
+  local bg_color = vim.api.nvim_get_hl(0, { name = 'Normal' }).bg
+  vim.wo[vim.fn.bufwinid(args.buf)].winhl = 'EndOfBuffer:NeoTreeEndOfBuffer'
+  vim.api.nvim_set_hl(0, 'NeoTreeEndOfBuffer', { fg = bg_color })
+end
+
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree position=right reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    {
+      '\\',
+      '<cmd>Neotree position=right reveal<cr>',
+      desc = 'NeoTree reveal',
+      silent = true,
+    },
   },
   opts = {
     filesystem = {
@@ -22,4 +33,11 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    require('neo-tree').setup(opts)
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'neo-tree',
+      callback = hide_eob,
+    })
+  end,
 }
