21d95b405d642ba7476dbd319b612710a722c315
Antoine Bagnaud <contact@antoinebagnaud.me>
2025-02-04 01:35:26 +0100

    Add elixir hooks
    See: https://github.com/frankdugan3/duganators-config.nvim/commit/f28c45b18c115d0209cb3306e74f663dd8913113

--- init.lua
+++ init.lua
@@ -889,7 +889,24 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'elixir',
+        'erlang',
+        'heex',
+        'surface',
+        'javascript',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
