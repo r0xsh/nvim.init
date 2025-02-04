33236ead030830b1855349b6cb30334a63eb2eb4
Antoine Bagnaud <contact@antoinebagnaud.me>
2025-02-04 06:29:14 +0100

    reorgenize lua

---
+++ lua/r0xsh/plugins/codeium.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'Exafunction/codeium.vim',
+    -- version = '1.8.37',
+    event = 'BufEnter',
+    config = function()
+      vim.g.codeium_idle_delay = 650
+      vim.keymap.set('i', '<c-;>', function()
+        return vim.fn['codeium#CycleCompletions'](1)
+      end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-,>', function()
+        return vim.fn['codeium#CycleCompletions'](-1)
+      end, { expr = true, silent = true })
+    end,
+  },
+}
---
+++ lua/r0xsh/plugins/lualine.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      local function get_codeium_status()
+        local status = vim.api.nvim_call_function('codeium#GetStatusString', {})
+        return '{…}' .. status
+      end
+      local marlin = require 'marlin'
+
+      local marlin_component = function()
+        local indexes = marlin.num_indexes()
+        if indexes == 0 then
+          return ''
+        end
+        local cur_index = marlin.cur_index()
+
+        return ' ' .. cur_index .. '/' .. indexes
+      end
+      require('lualine').setup {
+        options = {
+          icons_enabled = false,
+          -- theme = 'oxocarbon',
+          theme = 'jellybeans',
+          component_separators = '|',
+          section_separators = '',
+          refresh = {
+            statusline = 250,
+            tabline = 1000,
+            winbar = 1000,
+          },
+          disabled_filetypes = { 'neo-tree' },
+        },
+        sections = {
+          lualine_c = { 'filename', marlin_component },
+          -- lualine_y = { get_codeium_status },
+        },
+      }
+    end,
+  },
+}
