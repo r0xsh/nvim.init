19b2767a2bf02850a27c9a48db66a59ea7d842bb
Antoine Bagnaud <contact@antoinebagnaud.me>
2025-04-30 02:28:05 +0200

    set termguicolors

--- init.lua
+++ init.lua
@@ -164,6 +164,8 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 -- vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+vim.opt.termguicolors = true
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
